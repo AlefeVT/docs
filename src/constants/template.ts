cc81050812f858c2407ea8e42b7d359a202d6dd6
AlefeVT <alefevt@gmail.com>
2024-12-04 15:26:52 -0300

    feat: add template gallery component

---
+++ src/constants/template.ts
@@ -0,0 +1,37 @@
+export const templates = [
+  {
+    id: "blank",
+    label: "Documento em branco",
+    imageUrl: "/blank-document.svg",
+  },
+  {
+    id: "official-document",
+    label: "Ofício Formal",
+    imageUrl: "/oficio-formal.svg",
+  },
+  {
+    id: "technical-report",
+    label: "Relatório Técnico",
+    imageUrl: "/relatorio-tecnico.svg",
+  },
+  {
+    id: "internal-memo",
+    label: "Memorando Interno",
+    imageUrl: "/memorando-interno.svg",
+  },
+  {
+    id: "action-plan",
+    label: "Plano de Ação",
+    imageUrl: "/plano-de-acao.svg",
+  },
+  {
+    id: "official-statement",
+    label: "Comunicado Oficial",
+    imageUrl: "/official-statement.svg",
+  },
+  {
+    id: "official-municipality-correspondence",
+    label: "Correspondência Oficial",
+    imageUrl: "/official-municipality-correspondence.svg",
+  },
+];
